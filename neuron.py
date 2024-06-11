87f202c878467808bf873d2cd0b325dedd7b966a
Arthur Bied-Charreton <136271426+winstonallo@users.noreply.github.com>
2024-06-11 12:15:48 +0200

    Update neuron.py

--- neuron.py
+++ neuron.py
@@ -1,11 +1,10 @@
+import numpy as np
+
 class Neuron:
     def __init__(self, weights: list[float], bias: float) -> None:
         self.weights = weights
         self.bias = bias
 
     def output(self, inputs: list[float]) -> float:
-        # Summation of weighted inputs.
-        return (
-            sum(input_val * weight for input_val, weight in zip(inputs, self.weights))
-            + self.bias
-        )
+        # Summation of weighted inputs with added bias.
+        return np.dot(inputs, self.weights) + self.bias
