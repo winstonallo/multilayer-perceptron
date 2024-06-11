553c58f233406994ace4a7cb8de65e48cbb74baf
Arthur Bied-Charreton <136271426+winstonallo@users.noreply.github.com>
2024-06-11 12:16:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from neural_network import NeuralNetwork
 
-neural_network = NeuralNetwork(
-    weights=[[0.2, 0.8, -0.5, 1], [0.5, -0.91, 0.26, -0.5], [-0.26, -0.27, 0.17, 0.87]],
-    biases=[2, 3, 0.5],
-)
+weights1 = [2, 0, 0]  # First neuron's weights
+weights2 = [2, 2, 0]  # Second neuron's weights
+weights3 = [2, 2, 2]  # Third neuron's weights
 
-outputs = neural_network.run(inputs=[1, 2, 3, 2.5])
+neural_network = NeuralNetwork(weights=[weights1, weights2, weights3], biases=[0, 0, 1])
+
+outputs = neural_network.run(inputs=[1.1, 2.2, 3.3])
 print(outputs)
