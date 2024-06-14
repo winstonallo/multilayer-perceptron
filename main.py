26e4f38ceea603bf8eb68889888528410fe011bf
winstonallo <arthurbiedchar@gmail.com>
2024-06-14 17:29:40 +0200

    add: forward pass function

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
-from neural_network import NeuronLayer
+from neural_network import forward_pass
 import numpy as np
 
 inputs = [[1.0, 2.0, 3.0, 2.5], [2, 5, -1, 2], [-1.5, 2.7, 3.3, -0.8]]
 
-l1_weights = [[0.2, 0.8, -0.5, 1], [0.5, -0.91, 0.26, -0.5], [-0.26, -0.27, 0.17, 0.87]]
-l1_biases = [2.0, 3.0, 0.5]
+weights = [
+    np.array([[0.2, 0.8, -0.5, 1], [0.5, -0.91, 0.26, -0.5], [-0.26, -0.27, 0.17, 0.87]]),
+    np.array([[0.1, -0.14, 0.5], [-0.5, 0.12, -0.33], [-0.44, 0.73, -0.13]]),
+]
 
-l1 = NeuronLayer(weights=l1_weights, inputs=inputs, biases=l1_biases)
+biases = [[2.0, 3.0, 0.5], [-1, 2, -0.5]]
 
-l2_weights = [[0.1, -0.14, 0.5], [-0.5, 0.12, -0.33], [-0.44, 0.73, -0.13]]
-l2_biases = [-1, 2, -0.5]
+layers = forward_pass(inputs=np.array(inputs), weights=weights, biases=np.array(biases))
 
-l2 = NeuronLayer(weights=l2_weights, inputs=l1.output(), biases=l2_biases)
-
-
-output = l2.output()
-print(output)
-
+final_output = layers[-1].output()
+print(final_output)
--- neural_network.py
+++ neural_network.py
@@ -29,3 +29,14 @@ class NeuronLayer:
         # Inputs and weights are both X x Y, therefore we must transpose the weights
         # in order to convert them into Y x X and make the dot product possible.
         return np.dot(self.inputs, self.weights.T) + self.biases
+
+
+def forward_pass(inputs: np.ndarray, weights: np.ndarray, biases: np.ndarray) -> list[NeuronLayer]:
+    passes = len(weights)
+    layers = []
+    for i in range(passes):
+        if i == 0:
+            layers.append(NeuronLayer(weights=weights[i], inputs=inputs, biases=biases[i]))
+        else:
+            layers.append(NeuronLayer(weights=weights[i], inputs=layers[i - 1].output(), biases=biases[i]))
+    return layers
