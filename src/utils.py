9faa028742ea91a18a152789b1ef9d519c07a549
Arthur Bied-Charreton <136271426+winstonallo@users.noreply.github.com>
2024-06-18 07:53:46 +0200

    add: utils module

---
+++ src/utils.py
@@ -0,0 +1,11 @@
+from neural_network import NeuralNetwork
+import numpy as np
+from numpy import ndarray
+
+
+def evaluate_model(model: NeuralNetwork, x_test: ndarray, y_test: ndarray) -> float:
+    """
+    Evaluate the model on the test data and return the loss.
+    """
+    y_pred = model._forward(x_test)
+    return model.loss_func.forward(y_pred, y_test)
