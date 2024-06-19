5bfab78d794abfe8651e76dc064824f84b4f519e
Arthur Bied-Charreton <136271426+winstonallo@users.noreply.github.com>
2024-06-19 06:55:31 +0000

    add: all arguments

--- main.py
+++ main.py
@@ -12,33 +12,47 @@ parser = argparse.ArgumentParser(
     prog="multilayer-perceptron",
     description="This package allows easy customization, training, and performance evaluation of lightweight classification models.",
     epilog="Author: Arthur Bied-Charreton",
-    formatter_class=argparse.RawTextHelpFormatter
+    formatter_class=argparse.RawTextHelpFormatter,
 )
 
 group = parser.add_mutually_exclusive_group(required=True)
 group.add_argument(
-    "--from-pretrained", metavar="<path>", help="path to your trained model", action="store", type=str
+    "-p", "--from-pretrained", metavar="<path>", help="path to your trained model", action="store", type=str
 )
-group.add_argument("--optimized-training", help="tune and train a new model", action="store_true")
-group.add_argument("--custom-params", help="enter custom training parameters", action="store_true")
+group.add_argument("-t", "--tuned-parameters", help="tune and train a new model", action="store_true")
+group.add_argument("-c", "--custom-parameters", help="enter custom training parameters", action="store_true")
 
-parser.add_argument("data.csv", metavar="data you would like to train and evaluate your model with", help="path to input data", type=str)
+parser.add_argument("csv-path", metavar="<csv-path>", help="path to input data", type=str)
 
-parser.add_argument("--neurons", metavar="<int>", help="neurons count for hidden layers", action="store", type=int)
-parser.add_argument("--layers", metavar="<int>", help="number of dense layers", action="store", type=int)
 parser.add_argument(
+    "-n", "--neurons", metavar="<int>", help="neurons count for hidden layers", action="store", type=int
+)
+parser.add_argument("-l", "--layers", metavar="<int>", help="number of dense layers", action="store", type=int)
+parser.add_argument(
+    "-o",
     "--outputs",
     metavar="<int>",
     help="number of neurons in the output layer. use '--outputs 1' for binary classification",
     action="store",
-    type=int
+    type=int,
 )
-parser.add_argument("--learning-rate", metavar="<float>", help="learning rate for model training", action="store", type=float)
-parser.add_argument("--epochs", metavar="<int>", help="number of training iterations", action="store", type=int)
+parser.add_argument(
+    "-r", "--learning-rate", metavar="<float>", help="learning rate for model training", action="store", type=float
+)
+parser.add_argument("-e", "--epochs", metavar="<int>", help="number of training iterations", action="store", type=int)
+
+parser.add_argument(
+    "-s",
+    "--save",
+    metavar="<path>",
+    help="path to which you want to save the model.\nWARNING: use a non-existing directory, as this will clear any existing content",
+)
 
 args = parser.parse_args()
 
-if (args.from_pretrained or args.optimized_training) and (args.neurons or args.layers):
-    parser.error("--neurons and --layers are only valid with --custom-params")
+if (args.from_pretrained or args.optimized_training) and (
+    args.neurons or args.layers or args.outputs or args.learning_rate or args.epochs
+):
+    parser.error("--neurons, --layers, --outputs and --epochs are only valid with --custom-params")
 
 print(args)
